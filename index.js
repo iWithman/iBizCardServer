28f32ffddb6a9773e2705110ad4179b852d2c627
Withman Simprevil <simpw@mail.broward.edu>
2019-09-28 23:27:29 -0400

    no message

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const config = require('config');
 const mongoose = require('mongoose');
 const express =require('express');
 const cors = require('cors');
 const app = express();
 const cards = require('./routes/cards');
+const users = require('./routes/users');
+
+if(!config.get('myPrivateKey')) {
+  console.error('FATAL ERROR: myPrivateKey is not defined.');
+  process(1);
+}
 
 mongoose.connect('mongodb://localhost/iBusiness-Card', { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
